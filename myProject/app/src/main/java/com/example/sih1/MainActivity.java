f80012655ba23d57adf0c6174a376cbd0dd5c2e0
Anshu Singh <anshusingh12111997@gmail.com>
2020-07-07 22:59:31 +0530

    registration and login page added

--- myProject/app/src/main/java/com/example/sih1/MainActivity.java
+++ myProject/app/src/main/java/com/example/sih1/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.sih1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnSignIn, btnCreateAcc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSignIn = findViewById(R.id.btnSignIn);
+        btnCreateAcc = findViewById(R.id.btnCreateAcc);
+
+        btnCreateAcc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, UserRegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
