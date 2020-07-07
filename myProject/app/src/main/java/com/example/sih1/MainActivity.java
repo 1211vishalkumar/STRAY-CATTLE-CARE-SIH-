8c97e812946289ec98e76804c004a58d7110d8b8
1211vishalkumar <39715959+1211vishalkumar@users.noreply.github.com>
2020-07-07 23:07:46 +0530

    Authority Registration Page

--- myProject/app/src/main/java/com/example/sih1/MainActivity.java
+++ myProject/app/src/main/java/com/example/sih1/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.sih1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvAuth = findViewById(R.id.tvAuth);
+
+        tvAuth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,AuthorityRegister.class);
+                startActivity(intent);
+            }
+        });
     }
 }
